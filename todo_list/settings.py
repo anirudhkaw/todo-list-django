31aadfc4f8cf15ef54310b13aabcabfe644762c9
Anirudh Kaw <anikaw209@gmail.com>
2021-08-23 19:22:42 +0530

    adding static files

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -124,6 +124,11 @@ LOGIN_URL='login'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+
+
+]
 
 
 
